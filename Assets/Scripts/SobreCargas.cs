74bf737227b4678d3b61e5a37dc3204d1ac0d0cc
Pablo Correa <juanpabloc2000@gmail.com>
2023-05-19 17:09:01 -0500

    sobrecargas updated

--- Assets/Scripts/SobreCargas.cs
+++ Assets/Scripts/SobreCargas.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SobreCargas : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class SobreCargas : MonoBehaviour
     [SerializeField] private int min;
     [SerializeField] private int max;
     [SerializeField] private List<int> excludeList = new List<int>();
+    [SerializeField] private List<string> stringlist = new List<string>();
+    [SerializeField] private List<TextMeshProUGUI> textlist = new List<TextMeshProUGUI>();
 
     void Start()
     {
@@ -19,6 +22,11 @@ public class SobreCargas : MonoBehaviour
         excludeList.Add(number1 = ExcludeNumber(min, max, excludeList[0]));
         excludeList.Add(number2 = ExcludeNumber(min, max, excludeList));
         excludeList.Add(number3 = ExcludeNumber(min, max, excludeList));
+
+        textlist[0].text = stringlist[number];
+        textlist[1].text = stringlist[number1];
+        textlist[2].text = stringlist[number2];
+        textlist[3].text = stringlist[number3];
     }
 
 
